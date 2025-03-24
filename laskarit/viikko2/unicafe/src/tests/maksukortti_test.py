efb49052e19b9f1de48e6dda4108be03dafc87d6
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-03-24 17:05:19 +0200

    tehtävä 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,16 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_vahenee_oikein(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo, 500)
+    
+    def test_palauta_true_jos_tarpeeksi_saldoa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500),True)
+    def test_saldo_ei_vahene_jos_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(self.maksukortti.saldo, 1000)
+    
+    def test_palauta_false_jos__ei_tarpeeksi_saldoa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500),False)
