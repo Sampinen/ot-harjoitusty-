a4bded741b9ffa69034cfc7a872e5689acdd06ac
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-04-02 20:35:23 +0300

    autopep8 lisätty projektiin

--- TodoApp/src/tests/test_main.py
+++ TodoApp/src/tests/test_main.py
@@ -1,11 +1,13 @@
 import unittest
 import main
 
+
 class TestMain(unittest.TestCase):
     def test_pygame_window_opens(self):
         return
+
     def test_pygame_window_closes(self):
         return
-    def test_button_changes_color_when_pressed(self):
-        return 
 
+    def test_button_changes_color_when_pressed(self):
+        return
--- TodoApp/tasks.py
+++ TodoApp/tasks.py
@@ -16,3 +16,7 @@ def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
     if platform != "win32":
         call(("xdg-open", "htmlcov/index.html"))
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
