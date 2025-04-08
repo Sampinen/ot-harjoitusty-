f3838b25401bfbf445e2a1197cf5946d1f749c43
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-04-08 15:56:59 +0300

    Screen is changed when star button is pressed

--- TodoApp/src/firstscreen.py
+++ TodoApp/src/firstscreen.py
@@ -1,24 +1,16 @@
 import pygame
-from gamemenubuttons import MyButton
-#import pygame_widgets
-#from pygame_widgets.button import Button
-
-
-class GameMenu():
-
+from screen1buttons import MyButton
+class Screen1():
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((1280, 720))
         self.clock = pygame.time.Clock()
         self.running = False
-        self.start_button = None
-        self.create_start_button = False
-    def start_menu(self):
+
+    def run(self):
         #used code from pygame.org website as a layout
         self.running = True
-        self.screen.fill("gray")
-        self.create_start_button = True
-        start_game = False
+        self.screen.fill("blue")
         while self.running:
             # poll for events
             # pygame.QUIT event means the user clicked X to close your window
@@ -31,8 +23,10 @@ class GameMenu():
             
 
             #Pygame functions
+            FUNCS = {}
             # RENDER YOUR GAME HERE
-            start_button = MyButton(self.screen,"Aloita",10,10,True)
+            yes_button = MyButton(self.screen,"Kyllä",10,10,True)
+            no_button = MyButton(self.screen,"Ei",10,10,True)
             pygame.display.update()
 
             # flip() the display to put your work on screen
@@ -40,15 +34,4 @@ class GameMenu():
             pygame.display.flip()
             self.clock.tick(60)  # limits FPS to 60
         pygame.quit()
-
-
         return print("Program closed")
-
-
-    
-    def IsGameRunning(self):
-        return self.running
-    
-    def CloseGame(self):
-        self.running = False
-        return self.running
--- TodoApp/src/main.py
+++ TodoApp/src/main.py
@@ -1,11 +1,13 @@
 # Example file showing a basic pygame "game loop"
 import pygame
-import pygame_widgets as pw
-from pygame_widgets.button import Button
 from gamemenu import GameMenu
+from firstscreen import Screen1
 # pygame setup
 
 if __name__ == "__main__":
     StartMenu = GameMenu()
-    StartMenu.start_menu()
-    
+    menu =StartMenu.start_menu()
+    if menu == True:
+        Screen1()
+    else:
+        print("Program Closed")
