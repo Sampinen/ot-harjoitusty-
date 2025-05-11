dfdf919af147a13818878fab11a930c6cd10c85d
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-05-11 15:23:58 +0300

    lisätty docstringit draw_text tiedostolle

--- TodoApp/src/draw/draw_text.py
+++ TodoApp/src/draw/draw_text.py
@@ -1,3 +1,4 @@
+"""Draws text on screen with pygames' screen.blit function"""
 import pygame
 import texts.firstscreen_text as texts
 
@@ -9,38 +10,49 @@ def draw_text(screen,textt,x,y):
     return screen_blit
 
 def print_money(screen,money):
+    """Prints the amount of money on screen"""
     draw_text(screen,"Rahat(€): "+money,550,350)
 
 def draw_phase0_1(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen, texts.phase0_11(),10,10)
     draw_text(screen, texts.phase0_12(),10,25)
     draw_text(screen,texts.phase0_13(),10,40)
 
 def draw_phase1_1(screen,name):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.say_hi(name),10,10)
     draw_text(screen,texts.phase1_11(),10,25)
     draw_text(screen,texts.phase1_12(),10,40)
 
 def draw_phase2(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase2_1(),10,10)
 
 def draw_phase3_1(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase3_1(),10,10)
 
 def draw_phase3_2(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase3_2(),10,10)
 
 def draw_phase3_3(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase3_3(),10,10)
 
 def draw_phase3_4(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase3_4(),10,10)
 
 def draw_phase3_5(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase3_5(),10,10)
 
 def draw_phase3_6(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase3_6(),10,10)
 
 def draw_phase3_7(screen):
+    """Draws all corresponding text lines for phasex_y (x=phase, y= y_th subphase)"""
     draw_text(screen,texts.phase3_7(),10,10)
