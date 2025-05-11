508c7c4caead4a7eed063dfeee8ea43f07fd9ab0
Sampinen <siiri.kalliomaki@helsinki.fi>
2025-05-11 15:19:41 +0300

    päivitetty firstscreen_text tiedoston tektien docstringejä

--- TodoApp/src/texts/firstscreen_text.py
+++ TodoApp/src/texts/firstscreen_text.py
@@ -5,74 +5,74 @@ def say_hi(name):
     return "Hauska tavata "+ name +"!"
 
 def phase0_11():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text= "Tervetuloa peliin! "
     return text
 def phase0_12():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Tässä pelissä tavoitteenasi on kerätä mahdollisimman paljon rahaa."
     return text
 
 def phase0_13():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text= "Mikä on sinun nimesi?"
     return text
 
 def phase1_11():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Sinulla on pian synttärit, mutta pankkitililläsi ei ole senttiäkään."
     return text
 
 def phase1_12():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Tahtoisit ostaa itsellesi hampurilaisaterian"
     return text
 
 def phase1_13():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     return "Onnea peliin!"
 
 def phase2_1():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text ="Kerää pullo klikkaamalla, niin saat rahaa"
     return text
 
 def phase3_1():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text ="Terve, olemmeko tavanneet joskus aiemmin?"
     return text
 
 def phase3_2():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Aivan, sinäkö se olit joka ui puiston suihkulähteessä?"
     return text
 
 def phase3_3():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Ai, mitä sinulla oli sitten asiaa?"
     return text
 
 def phase3_4():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Tuntemattomaltako sinä kinuat rahaa?!"
     return text
 
 def phase3_5():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "En minä nyt ilmaiseksi ole sinulle rahaa antamassa!"
     return text
 
 def phase3_6():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Voin antaa sinulle pari euroa, jos voisit käydä ruokkimassa puluja puolestani"
     return text
 
 def phase3_7():
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text ="Okei, oli hauska tavata!"
     return text
 
 def phase4_1(name):
-    """text for phasex_y (x=phase, y= y_th line of that phase)"""
+    """text for phasex_yz (x=phase, y= y_th subphase, z=z_th line of the subphase)"""
     text = "Hei "+ name + "!"
     return text
